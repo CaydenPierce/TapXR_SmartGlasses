2f12c571e64e8bffddd9adb675520a89f9397a86
cayden <cpierc3@uwo.ca>
2024-04-21 15:05:01 -0400

    add tap xr support to glasses

--- android_app/app/src/main/java/com/teamopensmartglasses/convoscope/ConvoscopeService.java
+++ android_app/app/src/main/java/com/teamopensmartglasses/convoscope/ConvoscopeService.java
@@ -28,6 +28,7 @@ import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -35,6 +36,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.tapwithus.sdk.TapListener;
+import com.tapwithus.sdk.TapSdk;
+import com.tapwithus.sdk.TapSdkFactory;
+import com.tapwithus.sdk.airmouse.AirMousePacket;
+import com.tapwithus.sdk.mode.RawSensorData;
+import com.tapwithus.sdk.mode.TapInputMode;
+import com.tapwithus.sdk.mouse.MousePacket;
+import com.tapwithus.sdk.tap.Tap;
 import com.teamopensmartglasses.convoscope.events.GoogleAuthFailedEvent;
 import com.teamopensmartglasses.convoscope.events.GoogleAuthSucceedEvent;
 import com.teamopensmartglasses.convoscope.convoscopebackend.BackendServerComms;
@@ -67,6 +76,14 @@ public class ConvoscopeService extends SmartGlassesAndroidService {
     public final String TAG = "Convoscope_ConvoscopeService";
 
     private final IBinder binder = new LocalBinder();
+
+    //TapXR stuff
+    private TapSdk sdk;
+    private boolean startWithControllerMode = true;
+    private String lastConnectedTapAddress = "";
+
+    private int imuCounter;
+    private int devCounter;
 
     //our instance of the SGM library
     //public SGMLib sgmLib;
@@ -172,6 +189,20 @@ public class ConvoscopeService extends SmartGlassesAndroidService {
         getCurrentMode(this);
 
         this.aioConnectSmartGlasses();
+
+        //setup TapXR
+        imuCounter = 0;
+        devCounter = 0;
+
+        sdk = TapSdkFactory.getDefault(this);
+        sdk.enableDebug();
+        if (!startWithControllerMode) {
+            sdk.setDefaultMode(TapInputMode.text(), true);
+        }
+        sdk.registerTapListener(tapListener);
+        if (sdk.isConnectionInProgress()) {
+            Log.d(TAG, "A Tap is connecting");
+        }
     }
 
     public void sendSettings(JSONObject settingsObj){
@@ -281,6 +312,8 @@ public class ConvoscopeService extends SmartGlassesAndroidService {
         displayPollLoopHandler.removeCallbacks(displayRunnableCode);
         locationSendingLoopHandler.removeCallbacksAndMessages(this);
         EventBus.getDefault().unregister(this);
+        //kill tap xr sdk
+        sdk.unregisterTapListener(tapListener);
         super.onDestroy();
     }
 
@@ -1318,5 +1351,159 @@ public class ConvoscopeService extends SmartGlassesAndroidService {
 
     public void resetGoogleAuthRetryCount() {
         googleAuthRetryCount = 0;
+    }
+
+
+    private TapListener tapListener = new TapListener() {
+
+
+        @Override
+        public void onMouseInputReceived(@NonNull String tapIdentifier, @NonNull MousePacket data) {
+//            log(tapIdentifier + " mouse input received " + data.dx.getInt() + " " + data.dy.getInt() + " " + data.dt.getUnsignedLong() + " " + data.proximity.getInt());
+        }
+
+        @Override
+        public void onBluetoothTurnedOn() {
+            log("Bluetooth turned ON");
+        }
+
+        @Override
+        public void onBluetoothTurnedOff() {
+            log("Bluetooth turned OFF");
+        }
+
+        @Override
+        public void onTapStartConnecting(@NonNull String tapIdentifier) {
+            log("Tap started connecting - " + tapIdentifier);
+        }
+
+        @Override
+        public void onTapConnected(@NonNull String tapIdentifier) {
+            log("TAP connected " + tapIdentifier);
+            Tap tap = sdk.getCachedTap(tapIdentifier);
+            if (tap == null) {
+                log("Unable to get cached Tap");
+                return;
+            }
+
+            lastConnectedTapAddress = tapIdentifier;
+            log(tap.toString());
+            Log.d(TAG, "Telling tap to enter controller mode");
+            sdk.startControllerMode(tapIdentifier);
+
+//            adapter.removeItem(tapIdentifier);
+//
+//            TapListItem newItem = new TapListItem(tapIdentifier, itemOnClickListener);
+//            newItem.tapName = tap.getName();
+//            newItem.tapFwVer = tap.getFwVer();
+////            newItem.isInControllerMode = sdk.isInMode(tapIdentifier, TapSdk.MODE_CONTROLLER);
+//            adapter.addItem(newItem);
+        }
+
+        @Override
+        public void onTapDisconnected(@NonNull String tapIdentifier) {
+            log("TAP disconnected " + tapIdentifier);
+            //adapter.removeItem(tapIdentifier);
+        }
+
+        @Override
+        public void onTapResumed(@NonNull String tapIdentifier) {
+            log("TAP resumed " + tapIdentifier);
+            Tap tap = sdk.getCachedTap(tapIdentifier);
+            if (tap == null) {
+                log("Unable to get cached Tap");
+                return;
+            }
+
+            log(tap.toString());
+            //adapter.updateFwVer(tapIdentifier, tap.getFwVer());
+        }
+
+        @Override
+        public void onTapChanged(@NonNull String tapIdentifier) {
+            log("TAP changed " + tapIdentifier);
+            Tap tap = sdk.getCachedTap(tapIdentifier);
+            if (tap == null) {
+                log("Unable to get cached Tap");
+                return;
+            }
+
+            log("TAP changed " + tap);
+            //adapter.updateFwVer(tapIdentifier, tap.getFwVer());
+        }
+
+//        @Override
+//        public void onControllerModeStarted(@NonNull String tapIdentifier) {
+//            log("Controller mode started " + tapIdentifier);
+//            adapter.onControllerModeStarted(tapIdentifier);
+//        }
+//
+//        @Override
+//        public void onTextModeStarted(@NonNull String tapIdentifier) {
+//            log("Text mode started " + tapIdentifier);
+//            adapter.onTextModeStarted(tapIdentifier);
+//        }
+
+        @Override
+        public void onTapInputReceived(@NonNull String tapIdentifier, int data, int repeatData) {
+            //adapter.updateTapInput(tapIdentifier, data, repeatData);
+            log("TapInputReceived - " + tapIdentifier + ", " + data + ", repeatData = " + repeatData);
+            sendReferenceCard("TapXR", "Event received: " + Integer.toString(data));
+        }
+
+        @Override
+        public void onTapShiftSwitchReceived(@NonNull String tapIdentifier, int data) {
+            log("TapSwitchShiftReceived - " + tapIdentifier + ", " + data);
+            //adapter.updateTapSwitchShift(tapIdentifier, data);
+        }
+
+        @Override
+        public void onTapChangedState(@NonNull String tapIdentifier, int state) {
+            log(tapIdentifier + " changed state: " + state);
+        }
+
+        @Override
+        public void onError(@NonNull String tapIdentifier, int code, @NonNull String description) {
+            log("Error - " + tapIdentifier + " - " + code + " - " + description);
+        }
+
+
+        @Override
+        public void onAirMouseInputReceived(@NonNull String tapIdentifier, @NonNull AirMousePacket data) {
+//            log(tapIdentifier + " air mouse input received " + data.gesture.getInt());
+        }
+
+        @Override
+        public void onRawSensorInputReceived(@NonNull String tapIdentifier,@NonNull RawSensorData rsData) {
+            //RawSensorData Object has a timestamp, dataType and an array points(x,y,z).
+//            if (rsData .dataType == RawSensorData.DataType.Device) {
+//                // Fingers accelerometer.
+//                // Each point in array represents the accelerometer value of a finger (thumb, index, middle, ring, pinky).
+//                Point3 thumb = rsData.getPoint(RawSensorData.iDEV_INDEX);
+//                if (thumb != null) {
+//                    double x = thumb.x;
+//                    double y = thumb.y;
+//                    double z = thumb.z;
+//                }
+//                // Etc... use indexes: RawSensorData.iDEV_THUMB, RawSensorData.iDEV_INDEX, RawSensorData.iDEV_MIDDLE, RawSensorData.iDEV_RING, RawSensorData.iDEV_PINKY
+//            } else if (rsData.dataType == RawSensorData.DataType.IMU) {
+//                // Refers to an additional accelerometer on the Thumb sensor and a Gyro (placed on the thumb unit as well).
+//                Point3 gyro = rsData.getPoint(RawSensorData.iIMU_GYRO);
+//                if (gyro != null) {
+//                    double x = gyro.x;
+//                    double y = gyro.y;
+//                    double z = gyro.z;
+//                }
+//                // Etc... use indexes: RawSensorData.iIMU_GYRO, RawSensorData.iIMU_ACCELEROMETER
+//            }
+            // -------------------------------------------------
+            // -- Please refer readme.md for more information --
+            // -------------------------------------------------
+        }
+
+    };
+
+    private void log(String message) {
+        Log.d("NEW TAG BOI", message);
     }
 }
